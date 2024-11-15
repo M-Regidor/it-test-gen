c41046abc6c1f435d190ff3c83545fab63e6cebb
integgy <matthewregidor@gmail.com>
2024-11-15 13:53:08 -0800

    creat question json

--- it-test-gen/src/components/Question.jsx
+++ it-test-gen/src/components/Question.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react"
+
 const Question = () => {
+    const [selectedOption, setSelectedOption] = useState()
+
+    const handleOption = e => {
+        setSelectedOption(Number(e.target.value))
+        console.log(e.target.value)
+    }
+
+    const questionDetails = {
+        question: "Which command is used to check network connectivity?",
+        opitions: ["ipconfig", "ping", "tracert", "nslookup"],
+        anwser: 1
+    }
+
     return (
-        <h1>Question</h1>
+        <>
+            <h1>{questionDetails.question}</h1>
+            {questionDetails.opitions.map((item, idx) =>
+                <label>
+                    <input 
+                        type="radio"
+                        value={idx}
+                        checked={selectedOption === idx}
+                        onChange={handleOption}
+                    />
+                    {item}
+                </label>
+            )}
+        </>
     )
 }
 
